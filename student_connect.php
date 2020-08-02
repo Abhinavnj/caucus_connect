f97705c2781751c1cabd0be95cc937e172253eac
Abhinav Sirohi <abhinavnj@gmail.com>
2020-08-02 18:47:22 -0400

    clear files

--- student_connect.php
+++ student_connect.php
@@ -17,7 +17,7 @@
 </html>
 
 <?php
-    // test commit again again
+    // test commit again again again
     $first_name = filter_input(INPUT_POST,'first_name');
     $last_name = filter_input(INPUT_POST,'last_name');
     $email = filter_input(INPUT_POST,'email');
