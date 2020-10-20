1e1336915ce5df5892de5de9a7f215adcb4057d0
Abhinav Sirohi <abhinavnj@gmail.com>
2020-10-20 00:34:37 -0400

    bug fixes

---
+++ alumni_hidden.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Caucus Connect</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <style>
+        #myInput {
+            background-image: url('/icons-used/search.svg');
+            background-position: 10px 12px;
+            background-repeat: no-repeat;
+            width: 50%;
+            font-size: 16px;
+            padding: 12px 20px 12px 40px;
+            border: 1px solid #ddd;
+            margin-bottom: 12px;
+        }
+
+        #myTable tr.header {
+            background-color: rgb(187, 191, 194) !important;
+        }
+
+        /* #myTable tr.header, #myTable tr:hover { */
+        #myTable tr:hover {
+            background-color: rgba(0, 162, 255, 0.263);
+        }
+
+        #myTable {
+            font-size: 15px;
+        }
+
+        .table td, .table th {
+            padding: .2rem;
+        }
+
+        #myContent {
+            padding-left: 20px;
+            padding-right: 20px;
+        }
+
+        #tableDesc {
+            padding-top: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.html">Caucus Connect</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="container d-flex flex-column flex-md-row justify-content-between">
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-right" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-right" href="about.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-right" href="contact.html">Contact Us</a>
+                </li>
+                </ul>
+                <div class="start-button">
+                <a href="form_select.html" class="btn btn-outline-danger">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div id="myContent">
+        <h5 id="tableDesc">Here is a list of our alumni. Type below to filter this list.</h2>
+        
+        <input class="form-control" type="text" id="myInput" placeholder="Search for area of interest or school">
+
+        <hr>
+
+        <div style="overflow-x:auto;">
+            <table class="table table-bordered table-striped" id="myTable">
+                <thead>
+                    <tr class="header">
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>General Area of Interest</th>
+                        <th>Specific Area of Interest</th>
+                        <th>School</th>
+                        <th>Major</th>
+                        <th>Current Industry</th>
+                        <th>Company(s)</th>
+                    </tr>
+                </thead>
+
+                <?php
+                    // Connect to database
+                    $host = "secret";
+                    $dbusername = "secret";
+                    $dbpassword = "secret";
+                    $dbname = "secret";
+
+                    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+                    // Send error if connection error
+                    if ($conn-> connect_error) {
+                        die("Connection failed:". $conn-> connect_error);
+                    }
+
+                    // Get data
+                    $sql = "SELECT alum_id, first_name, last_name, gen_area, specific_area, school, major, industry, company FROM alumni";
+                    $result = $conn-> query($sql);
+                    $rnum = $result->num_rows;
+
+                    // Display data
+                    if ($rnum > 0) {
+                        while($row = $result-> fetch_assoc() ) {
+                            if ($row["alum_id"] != 101) {
+                                if (strcmp(substr($row["gen_area"], 0, 1), "-") == 0) {
+                                    $row["gen_area"] = substr($row["gen_area"], 2);
+                                }
+                                if (strcmp(substr($row["specific_area"], 0, 1), "-") == 0) {
+                                    $row["specific_area"] = substr($row["specific_area"], 2);
+                                }
+                                if (strpos($row["gen_area"], "*") !== false) {
+                                    $row["gen_area"] = str_replace("*", ", ", $row["gen_area"]);
+                                }
+                                if (strpos($row["specific_area"], "*") !== false) {
+                                    $row["specific_area"] = str_replace("*", ", ", $row["specific_area"]);
+                                }
+                                if (strpos($row["school"], "*") !== false) {
+                                    $row["school"] = str_replace("*", ", ", $row["school"]);
+                                }
+                                echo "<tr><td>". $row["alum_id"] ."</td><td>". $row["first_name"] ."</td><td>". $row["last_name"] ."</td><td>". $row["gen_area"] ."</td><td>". $row["specific_area"] ."</td><td>". $row["school"] ."</td><td>". $row["major"] ."</td><td>". $row["industry"] ."</td><td>". $row["company"] ."</td></tr>";
+                            }
+                        }
+                        echo "</table>";
+                    }
+                    else {
+                        echo "0 results";
+                    }
+
+                    // Close connection
+                    $conn-> close();
+                ?>
+            </table>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function(){
+            $("#myInput").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $("#myTable tr").filter(function() {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+    </script>
+</body>
+
+</html>
---
+++ student_connect_hidden.php
@@ -0,0 +1,280 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Record Inserted</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <h3>You will be contacted as soon as you are matched with one of our alumni!</h3>
+</body>
+</html> -->
+
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    // use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
+    
+    require './php-mail/Exception.php';
+    require './php-mail/PHPMailer.php';
+    require './php-mail/SMTP.php';
+
+    $first_name = filter_input(INPUT_POST,'first_name');
+    $last_name = filter_input(INPUT_POST,'last_name');
+    $email = filter_input(INPUT_POST,'email');
+    $phone = filter_input(INPUT_POST,'phone');
+    $area = filter_input(INPUT_POST,'area');
+    $school = filter_input(INPUT_POST,'school');
+    $priority = filter_input(INPUT_POST,'priority');
+    $alum_id = filter_input(INPUT_POST,'alum_id');
+    $gmail = filter_input(INPUT_POST,'gmail');
+    $toUpdate = filter_input(INPUT_POST,'toUpdate');
+
+    if ($alum_id == null) {
+        $alum_id = 101;
+    }
+
+    // Function to send email to alumni
+    // function sendmail($email, $alum_id, $connection, $first_name) {
+    function sendmail($email, $alum_id, $first_name) {
+        $host = "secret";
+        $dbusername = "secret";
+        $dbpassword = "secret";
+        $dbname = "secret";
+
+        $connection = new mysqli($host, $dbusername, $dbpassword, $dbname);
+        
+        if (mysqli_connect_error()) {
+            die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+        }
+        else {
+            $SELECT = "SELECT first_name, last_name, email FROM alumni WHERE alum_id = '{$alum_id}' LIMIT 1";
+            $result = $connection->query($SELECT);
+            if ($result->num_rows > 0) {
+                // output data of each row
+                $row = $result->fetch_assoc();
+                $alum_first = $row["first_name"];
+                $alum_last = $row["last_name"];
+                $alum_email = $row["email"];
+
+                // email data
+                // $email = "abhinavnj@gmail.com";
+                $to = $email;
+                $subject = "Caucus Connect Registration Information";
+                $message = "Hello {$first_name},\nHere is the alumni information you requested:\nName: {$alum_first} {$alum_last}\nEmail: {$alum_email}";
+                $message = wordwrap($message,70); // use wordwrap() if lines are longer than 70 characters
+                $headers = 'From: caucusconnect@gmail.com' . "\r\n" .
+                           'Reply-To: caucusconnect@gmail.com' . "\r\n" .
+                           'X-Mailer: PHP/' . phpversion();
+            
+                // send email
+                echo " You will shortly recieve an email with the contact information of the person you requested.";
+                mail($to, $subject, $message, $headers);
+            } else {
+                echo "0 results";
+            }
+        }
+    }
+
+    function sendgmail_alumni($alum_id, $first_name, $last_name, $email) {
+        $host = "secret";
+        $dbusername = "secret";
+        $dbpassword = "secret";
+        $dbname = "secret";
+
+        $connection = new mysqli($host, $dbusername, $dbpassword, $dbname);
+        if (mysqli_connect_error()) {
+            die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+        }
+        else {
+            $SELECT = "SELECT first_name, email FROM alumni WHERE alum_id = '{$alum_id}' LIMIT 1";
+            $result = $connection->query($SELECT);
+
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $alum_name = $row["first_name"];
+                $alum_email = $row["email"];
+
+                //email using PHPMailer
+                $mail = new PHPMailer(true);
+                $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output        
+                $mail->SMTPSecure = "ssl";                                  // Send using SMTP
+                $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+                $mail->Username   = 'caucusconnect@gmail.com';              // SMTP username
+                $mail->Password   = 'Secaucus07094';                        // SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                $mail->Port       = 465;
+
+                $mail->setFrom('caucusconnect@gmail.com', 'Caucus Connect');
+                $mail->addAddress($alum_email, $alum_name);
+                $mail->addReplyTo('caucusconnect@gmail.com', 'Caucus Connect');
+
+                // Content
+                $message = "Hello {$alum_name},<br>You should be contacted shortly! This student requested your information:<br>Name: {$first_name} {$last_name}<br>Email: {$email}";
+                $message = wordwrap($message,70);
+                $subject = "Caucus Connect Match Information";
+                $mail->isHTML(true);
+                $mail->Subject = $subject;
+                $mail->Body    = $message;
+                $mail->AltBody = $message;
+                // send email
+                $mail->send();
+            }
+        }
+    }
+
+    function sendgmail($email, $alum_id, $first_name) {
+        $host = "secret";
+        $dbusername = "secret";
+        $dbpassword = "secret";
+        $dbname = "secret";
+
+        $connection = new mysqli($host, $dbusername, $dbpassword, $dbname);
+        
+        if (mysqli_connect_error()) {
+            die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+        }
+        else {
+            $SELECT = "SELECT first_name, last_name, email FROM alumni WHERE alum_id = '{$alum_id}' LIMIT 1";
+            $result = $connection->query($SELECT);
+            if ($result->num_rows > 0) {
+                // output data of each row
+                $row = $result->fetch_assoc();
+                $alum_first = $row["first_name"];
+                $alum_last = $row["last_name"];
+                $alum_email = $row["email"];
+
+                //email PHPMailer
+                $mail = new PHPMailer(true);
+                $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+                // $mail->isSMTP();           
+                $mail->SMTPSecure = "ssl";                                 // Send using SMTP
+                $mail->Host       = 'smtp.gmail.com';                 // Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+                $mail->Username   = 'caucusconnect@gmail.com';              // SMTP username
+                $mail->Password   = 'Secaucus07094';                        // SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                $mail->Port       = 465;
+
+                $mail->setFrom('caucusconnect@gmail.com', 'Caucus Connect');
+                $mail->addAddress($email, $first_name);
+                $mail->addReplyTo('caucusconnect@gmail.com', 'Caucus Connect');
+
+                // Content
+                $message = "Hello {$first_name},<br>Here is the alumni information you requested:<br>Name: {$alum_first} {$alum_last}<br>Email: {$alum_email}";
+                $message = wordwrap($message,70);
+                $subject = "Caucus Connect Registration Information";
+                $mail->isHTML(true);
+                $mail->Subject = $subject;
+                $mail->Body    = $message;
+                $mail->AltBody = $message;
+                // send email
+                $mail->send();
+            } else {
+                echo "0 results";
+            }
+        }
+    }
+
+    try {
+
+    if (strpos($gmail, 'sboe.org') !== false || strpos($gmail, 'caucusconnect') !== false || strpos($gmail, 'abhinavnj') !== false) {
+
+
+        if (!empty($first_name) && !empty($last_name) && !empty($email) && !empty($phone) && !empty($area) && !empty($school)) {
+            $host = "secret";
+            $dbusername = "secret";
+            $dbpassword = "secret";
+            $dbname = "secret";
+
+            $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+            if (mysqli_connect_error()) {
+                die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+            }
+            else {
+                $SELECT = "SELECT email FROM student WHERE email = ? LIMIT 1";
+                $INSERT = "INSERT INTO student(first_name, last_name, email, phone, area, school, priority, alum_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+                // $INSERT = "INSERT INTO student(first_name, last_name, email, phone, area, school, priority) VALUES(?, ?, ?, ?, ?, ?, ?)"; //GOOD
+                
+                // if (mysqli_query($conn, $sql)) {
+                //     echo "success";
+                // }
+                // else {
+                //     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                // }
+
+                //Prepare statement
+                $stmt = $conn->prepare($SELECT);
+                $stmt->bind_param("s", $email);
+                $stmt->execute();
+                $stmt->bind_result($email);
+                $stmt->store_result();
+                $rnum = $stmt->num_rows;
+                // if ($rnum==0 && sendmail($email, $alum_id, $conn, $first_name)) {
+                if ($rnum==0) {
+                    if ($toUpdate == "0") {
+                        $stmt->close();
+                        $stmt = $conn->prepare($INSERT);
+                        $stmt->bind_param("sssssssi", $first_name, $last_name, $email, $phone, $area, $school, $priority, $alum_id);
+                        // $stmt->bind_param("sssssss", $first_name, $last_name, $email, $phone, $area, $school, $priority); //GOOD
+                        $stmt->execute();
+                        // sendmail($email, $alum_id, $conn, $first_name);
+                        sendgmail($email, $alum_id, $first_name);
+                        sendgmail_alumni($alum_id, $first_name, $last_name, $email);
+                        echo "You will shortly recieve an email with the contact information of the person you requested to connect with!";
+                    }
+                    else {
+                        echo "The email you entered is not in our system. You can create a new account using this email by resubmitting the form and selecting 'no' to updating information.";
+                    }
+                }
+                else {
+                    if ($email === $gmail && $toUpdate == "1") { // only allow update if google login matches entered email
+                        $UPDATE_INFO = "UPDATE student 
+                                        SET first_name='$first_name', last_name='$last_name', phone='$phone', area='$area', school='$school', priority='$priority', alum_id='$alum_id'
+                                        WHERE email='$email'";
+
+                        if ($conn->query($UPDATE_INFO) === TRUE) {
+                            echo "Record updated successfully.";
+                            sendgmail($email, $alum_id, $first_name);
+                            sendgmail_alumni($alum_id, $first_name, $last_name, $email);
+                        }
+                        else {
+                            echo "Error updating record: " . $conn->error;
+                        }
+                    }
+                    else {
+                        if ($toUpdate == "0") {
+                            echo "You selected no to updating information and you are using an email that has already been used to register.";
+                        }
+                        else {
+                            echo "Login email does not match the entered email.";
+                        }
+                    }
+                }
+                $stmt->close();
+                $conn->close();
+            }
+        }
+        else {
+            $message = "All fields are required";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+            die();
+        }
+    }
+    else {
+        $message = "Must be signed in with sboe.org email address!";
+        echo "<script type='text/javascript'>alert('$message');</script>";
+        die();
+    }
+    }
+    catch (Exception $e) {
+            echo 'caught exception: ', $e->getMessage(), "\n";
+    }
+?>
